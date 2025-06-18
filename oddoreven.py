519e3560f1bc32f044e6066651c938a7215ce2ae
Nikhil76785 <valivetinikhil2@gmail.com>
2025-06-18 20:00:32 +0530

    ODD OR EVEN

---
+++ oddoreven.py
@@ -0,0 +1,8 @@
+number = int(input("Enter the number to check: "))
+print(f"Number to be checked: {number}")
+
+if (number % 2 == 0):
+    print(f"{number} is an even number")
+
+else:
+    print(f"{number} is an odd number")

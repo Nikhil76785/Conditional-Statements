6c3d69a7013af5b60d4796395c0c1243a0f6a0fb
Nikhil76785 <valivetinikhil2@gmail.com>
2025-06-18 20:17:55 +0530

    WEATHER

---
+++ clothes.py
@@ -0,0 +1,7 @@
+temp = int(input("Enter the temperature in Celsius: "))
+
+if (temp > 17, temp < 50):
+    print("You can wear light-colored, loose-fitting clothes made from breathable fabrics like cotton and linen.")
+
+else:
+    print("You can wear outerwear like long coats, coats,socks caps, scarves and gloves made from wool.")
